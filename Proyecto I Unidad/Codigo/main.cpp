42041204575c77d633969995063b8e5eac2f7e43
kennethowl <kennethowl00@gmail.com>
2020-09-29 22:10:07 -0600

    proyecto

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -7,9 +7,29 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     system("cls");
+      
+    int opcion=0;
+    
 
-    // starShip();
-    // snake();
+    while(true){
+    cout << "Ingrese una opcion: " << endl;
+    cin >> opcion;
+    cout << "1 - Startship" << endl;
+    cout << "2 - Snake" << endl;
 
+    switch{
+
+    case 1: starShip();
+    break;
+    case 2: snake();
+    break;
+    default:
+    break;
+    }
+    }
+
+    if (opcion=0){
+        cout << "Fin del Programa" << endl;
+    }
     return 0;
 }
